1e91b1d5c0451f8ff907da4afaad47455bd2391a
Raj Prabhakar <91955195+raj-prabhakar@users.noreply.github.com>
2024-09-08 17:19:13 +0530

    u

--- forever-full-stack/frontend/src/components/Footer.jsx
+++ forever-full-stack/frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
         <div>
-            <img src={assets.logo} className='w-34' alt="" />
+            <img src={assets.logo} className='w-32' alt="" />
             <p className='w-full md:w-2/3 text-gray-600'>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
@@ -27,7 +27,7 @@ const Footer = () => {
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
                 <li>+1-212-456-7890</li>
-                <li>contact@TrendHaven.com</li>
+                <li>contact@Forever.com</li>
             </ul>
         </div>
 
@@ -35,7 +35,7 @@ const Footer = () => {
 
         <div>
             <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2024@ TrendHaven.com - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2024@ Forever.com - All Right Reserved.</p>
         </div>
 
     </div>
